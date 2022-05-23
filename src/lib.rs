1c1772090ce3fc74eae924f65de0d43a57efffbc
smsutherland <sagan@farthinghalearms.com>
2022-05-22 23:36:54 -0400

    document Function

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,81 @@
+//! A mathematical library for creating, operating on, and evaluating functions.
+
 use std::fmt::Display;
 
 pub use std::f64::consts::{E, PI};
 
+/// The core data type of the crate.
+///
+/// A `Function` can be evaluated for a given value using [`eval`].
+///
+/// [`eval`]: fn@crate::Function::eval
 #[derive(Debug, Clone)]
 pub enum Function {
+    /// A numeric literal. All literal values are represented as 64-bt floats.
     Lit(f64),
+    /// A binary operation (ex. `f(x) + g(x)`).
     BinaryOp(Box<Function>, BinaryOperator, Box<Function>),
+    /// A unary operation (ex. `-f(x)`).
     UnaryOp(UnaryOperator, Box<Function>),
+    /// A builtin function such as `sin`.
     Builtin(BuiltinFunction),
+    /// A composition of functions (ex. `f(g(x))`).
     Composition(Box<Function>, Box<Function>),
+    /// Represents the variable in a function (ex. the `x` in `x^2`).
+    /// In a composition of functions, `Variable` inside the outer function represents the entire inner function.
     Variable,
 }
 
+/// Enumeration of all errors which can occur while evaluating a function.
 #[derive(Debug)]
 pub enum FunctionError {
+    /// Caused by division by zero.
     DivideByZero,
+    /// Caused by `0^0`.
     ZeroToZero,
 }
 
 impl Function {
-    pub fn eval(&self, input: f64) -> Result<f64, FunctionError> {
+    /// Evaluate the function with the given value.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if an illegal operation takes place during evaluation.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use function::{Function, BinaryOperator};
+    /// // x^2 - 1
+    /// let func = Function::BinaryOp(
+    ///     Box::new(Function::BinaryOp(
+    ///         Box::new(Function::Variable),
+    ///         BinaryOperator::Pow,
+    ///         Box::new(Function::Lit(2.0)),
+    ///     )),
+    ///     BinaryOperator::Minus,
+    ///     Box::new(Function::Lit(1.0)),
+    /// );
+    /// assert_eq!(func.eval(2.0).unwrap(), 3.0);
+    /// ```
+    pub fn eval(&self, value: f64) -> Result<f64, FunctionError> {
         match self {
             Self::Lit(literal) => Ok(*literal),
             Self::BinaryOp(first, op, second) => {
-                let first_value = first.eval(input)?;
-                let second_value = second.eval(input)?;
+                let first_value = first.eval(value)?;
+                let second_value = second.eval(value)?;
                 op.eval(first_value, second_value)
             }
             Self::UnaryOp(op, operand) => {
-                let operand_value = operand.eval(input)?;
+                let operand_value = operand.eval(value)?;
                 op.eval(operand_value)
             }
-            Self::Builtin(builtin) => builtin.eval(input),
+            Self::Builtin(builtin) => builtin.eval(value),
             Self::Composition(outer, inner) => {
-                let inner_value = inner.eval(input)?;
+                let inner_value = inner.eval(value)?;
                 outer.eval(inner_value)
             }
-            Self::Variable => Ok(input),
+            Self::Variable => Ok(value),
         }
     }
 
@@ -61,6 +100,27 @@ impl Function {
         }
     }
 
+    /// Returns a composition of functions in which `self` is the outer function and `inner` is the inner function.
+    ///
+    /// # Examples
+    /// ```
+    /// # use function::{Function, BuiltinFunction, BinaryOperator};
+    /// // sin(x)
+    /// let inner = Function::Composition(
+    ///     Box::new(Function::Builtin(BuiltinFunction::Sin)),
+    ///     Box::new(Function::Variable),
+    /// );
+    /// // x^2
+    /// let outer = Function::BinaryOp(
+    ///     Box::new(Function::Variable),
+    ///     BinaryOperator::Pow,
+    ///     Box::new(Function::Lit(2.0)),
+    /// );
+    /// // sin^2(x)
+    /// let func = outer.compose(&inner);
+    /// let result = f64::sin(3.0) * f64::sin(3.0);
+    /// assert_eq!(func.eval(3.0).unwrap(), result);
+    /// ```
     pub fn compose(&self, inner: &Self) -> Self {
         Self::Composition(Box::new(self.clone()), Box::new(inner.clone()))
     }
